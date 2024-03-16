886124041b5da8869ee74a432bf70775a71b64fe
jayancv <jcvidanapathirana@gmail.com>
2024-03-16 10:35:40 +0200

    Tokenize comments and token position

--- src/compiler/tokenizer.py
+++ src/compiler/tokenizer.py
@@ -2,13 +2,15 @@ import re
 from dataclasses import dataclass
 from typing import Literal
 
-TokenType = Literal["int_literal", "operators", "identifier", "parenthesis", "end"]
+TokenType = Literal["int_literal", "operators", "identifier", "parenthesis", "comment", "end"]
 
 
 @dataclass(frozen=True)
 class Token:
     type: TokenType
     text: str
+    line: int
+    column: int
 
 
 def tokenize(source_code: str) -> list[Token]:
@@ -17,37 +19,101 @@ def tokenize(source_code: str) -> list[Token]:
     identifier_rex = re.compile(r'[a-zA-Z_][a-zA-Z0-9_]*')
     operator_rex = re.compile(r'[<>*/+-]')
     parenthesis_rex = re.compile(r'[(){}]')
+    single_line_comment_rex = re.compile(r'//.*?(?:\n|$)')
+    multi_line_comment_rex = re.compile(r'/\*.*?\*/', re.DOTALL)
 
     position = 0
+    line_num = 1
+    column_num = 1
     result: list[Token] = []
+    in_multi_line_comment = False
 
     while position < len(source_code):
+
         match = whitespace_rex.match(source_code, position)
         if match is not None:
+            if '\n' in match.group():
+                lines = match.group().split('\n')
+                line_num += len(lines) - 1
+                column_num = len(lines[-1]) + 1
+            else:
+                column_num += len(match.group())
             position = match.end()
             continue
 
+        if not in_multi_line_comment:
+            # Match single-line comments
+            match = single_line_comment_rex.match(source_code, position)
+            if match is not None:
+                # result.append(Token(type='comment', text=match.group(), line=line_num, column=column_num))
+                if '\n' in match.group():
+                    lines = match.group().split('\n')
+                    line_num += len(lines) - 1
+                    column_num = len(lines[-1]) + 1
+                else:
+                    column_num += len(match.group())
+                position = match.end()
+                continue
+
+        # Match multi-line comments
+        match = multi_line_comment_rex.match(source_code, position)
+        if match is not None:
+            if not in_multi_line_comment:
+                in_multi_line_comment = True
+            if '\n' in match.group():    # get all lines
+                lines = match.group().split('\n')
+                for line in lines[:-1]:
+                    line_num += 1
+                    column_num = 1
+                if lines[-1]:           # last line
+                    column_num = len(lines[-1]) - 2
+            else:                         # single line
+                column_num += len(match.group())
+            position = match.end()-2
+            continue
+
+        # Check if multi-line comment ends
+        if in_multi_line_comment:
+            end_match = re.search(r'\*/', source_code[position:])
+            if end_match:
+                end_index = position + end_match.start() + 2  # Move to the end of '*/'
+                line_num += source_code[position:end_index].count('\n')
+                if '\n' in source_code[position:end_index]:
+                    column_num = len(source_code[position:end_index].split('\n')[-1]) + 1
+                else:
+                    column_num += end_match.start()
+                position = end_index
+                in_multi_line_comment = False
+                continue
+            else:
+                position = len(source_code)
+                continue
+
         match = identifier_rex.match(source_code, position)
         if match is not None:
-            result.append(Token(type='identifier', text=source_code[position:match.end()]))
+            result.append(Token(type='identifier', text=source_code[position:match.end()], line=line_num, column=column_num))
+            column_num += len(match.group())
             position = match.end()
             continue
 
         match = integer_rex.match(source_code, position)
         if match is not None:
-            result.append(Token(type='int_literal', text=source_code[position:match.end()]))
+            result.append(Token(type='int_literal', text=source_code[position:match.end()], line=line_num, column=column_num))
+            column_num += len(match.group())
             position = match.end()
             continue
 
         match = operator_rex.match(source_code, position)
         if match is not None:
-            result.append(Token(type='operators', text=source_code[position:match.end()]))
+            result.append(Token(type='operators', text=source_code[position:match.end()], line=line_num, column=column_num))
+            column_num += len(match.group())
             position = match.end()
             continue
 
         match = parenthesis_rex.match(source_code, position)
         if match is not None:
-            result.append(Token(type='parenthesis', text=source_code[position:match.end()]))
+            result.append(Token(type='parenthesis', text=source_code[position:match.end()], line=line_num, column=column_num))
+            column_num += len(match.group())
             position = match.end()
             continue
 
--- tests/tokenizer_test.py
+++ tests/tokenizer_test.py
@@ -3,35 +3,50 @@ from compiler.tokenizer import tokenize, Token
 
 def test_tokenizer() -> None:
     assert tokenize("Hello") == [
-        Token(type='identifier', text='Hello')
+        Token(type='identifier', text='Hello', line=1, column=1)
     ]
     assert tokenize("  \n Hello         \n    \n") == [
-        Token(type='identifier', text='Hello')
+        Token(type='identifier', text='Hello', line=2, column=2)
     ]
 
     assert tokenize("  \n Hello    I am Jayan 123     \n    \n") == [
-        Token(type='identifier', text='Hello'),
-        Token(type='identifier', text="I"),
-        Token(type='identifier', text="am"),
-        Token(type='identifier', text='Jayan'),
-        Token(type='int_literal', text='123')
+        Token(type='identifier', text='Hello', line=2, column=2),
+        Token(type='identifier', text="I", line=2, column=11),
+        Token(type='identifier', text="am", line=2, column=13),
+        Token(type='identifier', text='Jayan', line=2, column=16),
+        Token(type='int_literal', text='123', line=2, column=22)
     ]
 
     assert tokenize(" 3+ -6+2-(1)") == [
-        Token(type='int_literal', text='3'),
-        Token(type='operators', text='+'),
-        Token(type='operators', text='-'),
-        Token(type='int_literal', text='6'),
-        Token(type='operators', text='+'),
-        Token(type='int_literal', text='2'),
-        Token(type='operators', text='-'),
-        Token(type='parenthesis', text='('),
-        Token(type='int_literal', text='1'),
-        Token(type='parenthesis', text=')'),
+        Token(type='int_literal', text='3', line=1, column=2),
+        Token(type='operators', text='+', line=1, column=3),
+        Token(type='operators', text='-', line=1, column=5),
+        Token(type='int_literal', text='6', line=1, column=6),
+        Token(type='operators', text='+', line=1, column=7),
+        Token(type='int_literal', text='2', line=1, column=8),
+        Token(type='operators', text='-', line=1, column=9),
+        Token(type='parenthesis', text='(', line=1, column=10),
+        Token(type='int_literal', text='1', line=1, column=11),
+        Token(type='parenthesis', text=')', line=1, column=12),
     ]
 
     assert tokenize("if  3\nwhile") == [
-        Token(type='identifier', text='if'),
-        Token(type='int_literal', text='3'),
-        Token(type='identifier', text='while')
+        Token(type='identifier', text='if', line=1, column=1),
+        Token(type='int_literal', text='3', line=1, column=5),
+        Token(type='identifier', text='while', line=2, column=1)
+    ]
+
+    assert tokenize("  \n Hello         \n  // Comment  \n jayan") == [
+        Token(type='identifier', text='Hello', line=2, column=2),
+        Token(type='identifier', text='jayan', line=4, column=2)
+    ]
+
+    assert tokenize("  \n Hello     /* multi \n line \n comment */\n  Jayan  \n    \n") == [
+        Token(type='identifier', text='Hello', line=2, column=2),
+        Token(type='identifier', text='Jayan', line=5, column=3)
+    ]
+
+    assert tokenize("  \n Hello     /* multi  line single comment */  Jayan  \n    \n") == [
+        Token(type='identifier', text='Hello', line=2, column=2),
+        Token(type='identifier', text='Jayan', line=2, column=46)
     ]
